25f89aa95216dff293fcc85d11ebe9478ad182e9
lambricm <lambricm@mail.uc.edu>
2016-04-17 09:29:00 -0400

    Added some note bullshit

--- MentalMusic/src/mentalMusic/MuseReader.java
+++ MentalMusic/src/mentalMusic/MuseReader.java
@@ -18,5 +18,46 @@ public class MuseReader {
 	}
 	
 	
+	public Double findNearestNote(Double dbl){
+		dbl = dbl/2; //divide by 2 for regular notes
+		
+		Integer num_div = 0;
+		while (dbl > 246.94 || dbl < 246.94){
+			dbl = dbl/2;
+			num_div++;
+		}
+		
+		if(dbl < 0){
+			num_div *= (-1);
+		}
+		
+		if(dbl < 138.59){
+			dbl = 130.82;
+		}
+		else if(dbl < 155.56){
+			dbl = 146.83;
+		}
+		else if(dbl < 169.71){
+			dbl = 164.81;
+		}
+		else if(dbl < 185.00){
+			dbl = 174.61;
+		}
+		else if(dbl < 207.65){
+			dbl = 196.00;
+		}
+		else if(dbl < 233.08){
+			dbl = 220.00;
+		}
+		else{
+			dbl = 246.94;
+		}
+		
+		return dbl*num_div;
+	}
 
+	private Integer valueOf(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
