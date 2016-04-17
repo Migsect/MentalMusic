8c193120383eab4f8f4e884ca66cb0fa69d62eb9
lavinrp <lavinrp@gmail.com>
2016-04-17 00:45:31 -0400

    changed to a different OSC. NOW WITH LESS SHIT!!

--- MentalMusic/src/MentalMusic.java
+++ MentalMusic/src/MentalMusic.java
@@ -6,11 +6,11 @@ public class MentalMusic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MuseReader mr = new MuseReader();
-		while (true) {
+		//while (true) {
 			//Muse-A168
 			
 			System.out.println(mr.connectToMuse("/muse/eeg/quantization"));
-		}
+		
 		//System.out.print("bla");
 	}
 
--- MentalMusic/src/MuseReader.java
+++ MentalMusic/src/MuseReader.java
@@ -1,36 +1,38 @@
 import java.net.SocketException;
 import java.util.Date;
-
-import com.illposed.osc.OSCListener;
-import com.illposed.osc.OSCMessage;
-import com.illposed.osc.OSCPort;
-import com.illposed.osc.OSCPortIn;
+import oscP5.*;
 
 /*Read data from a muse and output to a given */
 public class MuseReader {
-	public static final int MUSE_PORT = 5000;
+	
+	
+	
+	static int recvPort = 5000;
+
+	class MuseOscServer {
+		OscP5 museServer;
+		MuseOscServer museOscServer;
+		void oscEvent(OscMessage msg) {
+			System.out.println("### got a message " + msg);
+			if (msg.checkAddrPattern("/muse/eeg")==true) {  
+				for(int i = 0; i < 4; i++) {
+					System.out.print("EEG on channel " + i + ": " + msg.get(i).floatValue() + "\n"); 
+				}
+			} 
+		}
+	}
+
 	MuseReader(){
 		//TODO: fill stub
 	}
 	
 	public boolean connectToMuse(String museName) {
-		try{
-			OSCPortIn receiver = new OSCPortIn(MUSE_PORT);
-			OSCListener listener = new OSCListener() {
-				public void acceptMessage(java.util.Date time, OSCMessage message) {
-					System.out.println(message.getCharset());
-				}
-			};
-			receiver.addListener(museName, listener);
-			receiver.startListening();
-			//receiver.wait
-			return true;
-		} catch(Exception e) {
-			//TODO: fix this
-			return false;
-		}
+		MuseOscServer museOscServer = new MuseOscServer();
+		museOscServer.museServer = new OscP5(museOscServer, recvPort);
+		
+		museOscServer.oscEvent(museOscServer.museServer.newMsg(museName));
+		
+		return false;
 	}
-	
-	
 
 }
