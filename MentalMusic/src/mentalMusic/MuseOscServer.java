235f0eafa31b3e5b9f3257962453cdae75619fba
lavinrp <lavinrp@gmail.com>
2016-04-17 06:15:28 -0400

    We have some sound!!!

--- MentalMusic/src/mentalMusic/MuseOscServer.java
+++ MentalMusic/src/mentalMusic/MuseOscServer.java
@@ -1,5 +1,9 @@
 package mentalMusic;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+
 import oscP5.*;
 
 public class MuseOscServer {
@@ -26,7 +30,24 @@ public class MuseOscServer {
 		System.out.println("### got a message " + msg);
 		if (msg.checkAddrPattern("/muse/eeg")==true) {  
 			for(int i = 0; i < 4; i++) {
-				System.out.print("EEG on channel " + i + ": " + msg.get(i).floatValue() + "\n"); 
+				try{
+					byte[] buf = new byte[ 1 ];
+				    AudioFormat af = new AudioFormat( (float )44100, 8, 1, true, false );
+				    SourceDataLine sdl = AudioSystem.getSourceDataLine( af );
+				    sdl.open();
+				    sdl.start();
+				    for( int k = 0; k < 1000 * (float )44100 / 1000; k++ ) {
+				        //double angle = k / ( (float )44100 / 440 ) * 2.0 * Math.PI;
+				    	
+				        buf[ 0 ] = (byte )( Math.sin( msg.get(i).floatValue() ) * 100 );
+				        sdl.write( buf, 0, 1 );
+				    }
+				    sdl.drain();
+				    sdl.stop();
+				} catch (Exception e) {
+					System.out.println("boo");
+				}
+				//System.out.print("EEG on channel " + i + ": " + msg.get(i).floatValue() + "\n"); 
 			}
 		} 
 	}
