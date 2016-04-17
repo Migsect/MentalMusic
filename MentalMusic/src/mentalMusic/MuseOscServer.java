e038c52f59b3f0463d569f3c3cbcd70ae903d217
lavinrp <lavinrp@gmail.com>
2016-04-17 10:36:49 -0400

    can now play 5ever

--- MentalMusic/src/mentalMusic/MuseOscServer.java
+++ MentalMusic/src/mentalMusic/MuseOscServer.java
@@ -29,28 +29,24 @@ public class MuseOscServer {
 	void oscEvent(OscMessage msg) {
 		//System.out.println("### got a message " + msg);
 		if (msg.checkAddrPattern("/muse/eeg")==true) {  
-			//for(int i = 0; i < 4; i++) {
-				try{
-					byte[] buf = new byte[ 1 ];
-				    AudioFormat af = new AudioFormat( (float )44100, 8, 1, true, false );
-				    SourceDataLine sdl = AudioSystem.getSourceDataLine( af );
-				    sdl.open();
-				    sdl.start();
-				    int i = 0;
-				    while(true){//for( int i = 0; i < 1000 * (float )44100 / 1000; i++ ) {
-				        double angle = 500 / ( (float )44100 / msg.get(i).floatValue()/*440*/ ) * 2.0 * Math.PI;
-				        buf[ 0 ] = (byte )( Math.sin( angle ) * 1000 );
-				        sdl.write( buf, 0, 1 );
-				        System.out.print("EEG on channel " + i + ": " + msg.get(i).floatValue() + "\n"); 
-				        if (i < 1000000000) break;
-				    }
-				    sdl.drain();
-				    sdl.stop();
-				} catch (Exception e) {
-					System.out.println("boo");
-				}
-				
-			//}
+			try{
+				byte[] buf = new byte[ 1 ];
+			    AudioFormat af = new AudioFormat( (float )44100, 8, 1, true, false );
+			    SourceDataLine sdl = AudioSystem.getSourceDataLine( af );
+			    sdl.open();
+			    sdl.start();
+			    int i = 0;
+			    
+		        double angle = 500 / ( (float )44100 / msg.get(i).floatValue()/*440*/ ) * 2.0 * Math.PI;
+		        buf[ 0 ] = (byte )( Math.sin( angle ) * 1000 );
+		        sdl.write( buf, 0, 1 );
+		        System.out.print("EEG on channel " + 0 + ": " + msg.get(0).floatValue() + "\n"); 
+
+			    sdl.drain();
+			    sdl.stop();
+			} catch (Exception e) {
+				System.out.println("boo");
+			}
 		} 
 	}
 
