3a365036fc2b1443b7d3f315fcdbb88470406545
lambricm <lambricm@mail.uc.edu>
2016-04-16 22:26:04 -0400

    did some shiiiit

--- MentalMusic/src/MentalMusic.java
+++ MentalMusic/src/MentalMusic.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
+import GUI.Window;
+
 
 public class MentalMusic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		Window musicWindow = new Window();
 	}
 
 }
