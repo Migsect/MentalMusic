fd2cb7e9f291f2e08d7c9b2f0d9f4637d5999f73
lavinrp <lavinrp@gmail.com>
2016-04-17 01:01:11 -0400

    Added MusicOscServer class. updated some random eclipse stuff that we still haven't removed.

---
+++ MentalMusic/src/MuseOscServer.java
@@ -0,0 +1,32 @@
+
+import oscP5.*;
+
+public class MuseOscServer {
+
+	static MuseOscServer museOscServer;
+	
+	OscP5 museServer;
+	static int recvPort = 5000;
+
+	
+	
+	/*public static void main(String [] args) {
+		museOscServer = new MuseOscServer();
+		museOscServer.museServer = new OscP5(museOscServer, recvPort);
+			}*/
+	
+	public void connectToMuse(String museName) {
+		//TODO: make this a bool that returns false if stuff breaks
+		museOscServer = new MuseOscServer();
+		museOscServer.museServer = new OscP5(museOscServer, recvPort);
+	}
+	
+	void oscEvent(OscMessage msg) {
+		System.out.println("### got a message " + msg);
+		if (msg.checkAddrPattern("/muse/eeg")==true) {  
+			for(int i = 0; i < 4; i++) {
+				System.out.print("EEG on channel " + i + ": " + msg.get(i).floatValue() + "\n"); 
+			}
+		} 
+	}
+}
