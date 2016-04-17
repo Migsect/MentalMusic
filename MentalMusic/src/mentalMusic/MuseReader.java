afcd78fe655fdf14b7041b44dd0086d2dfea30ba
lambricm <lambricm@mail.uc.edu>
2016-04-17 13:54:59 -0400

    did some shit

--- MentalMusic/src/mentalMusic/MuseReader.java
+++ MentalMusic/src/mentalMusic/MuseReader.java
@@ -21,10 +21,10 @@ public class MuseReader {
 	public static Double findNearestNote(Double dbl){
 		//dbl = dbl/2; //divide by 2 for regular notes
 		
-		Integer low_num = 550;
-		Integer high_num = 1150;
+		Integer low_num = 550*3;
+		Integer high_num = 1150*3;
 		Integer diff = high_num - low_num;
-		Integer poss_val = 7;
+		Integer poss_val = 21*3;
 		
 		Integer num_div = 0;
 		Integer num_mult = 0;
@@ -69,8 +69,38 @@ public class MuseReader {
 				return 196.00;
 			case 6:
 				return 220.00;
+			case 7:
+				return 261.63;
+			case 8:
+				return 293.66;
+			case 9:
+				return 329.63;
+			case 10:
+				return 349.23;
+			case 11:
+				return 392.00;
+			case 12:
+				return 440.00;
+			case 13:
+				return 493.88;
+			case 14:
+				return 523.25;
+			case 15:
+				return 587.33;
+			case 16:
+				return 659.26;
+			case 17:
+				return 698.46;
+			case 18:
+				return 783.99;
+			case 19:
+				return 880.00;
+			case 20:
+				return 987.77;
+			case 21:
+				return 1046.50;
 			default:
-				return 246.94;
+				return 1174.66;
 							
 		}
 		
