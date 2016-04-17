a12666007ae4b15e0cab2b2b6023b96afb06acb6
lavinrp <lavinrp@gmail.com>
2016-04-16 22:54:47 -0400

    Some working stuff. Still lots of random eclipse stuff.

--- MentalMusic/src/MentalMusic.java
+++ MentalMusic/src/MentalMusic.java
@@ -5,8 +5,13 @@ public class MentalMusic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.print("bla");
+		MuseReader mr = new MuseReader();
+		while (true) {
+			//Muse-A168
+			mr.connectToMuse("/muse/eeg/quantization");
+			System.out.println("stuff");
+		}
+		//System.out.print("bla");
 	}
 
 }
